0930ad2b3521dc0bc650608ae3affb20a35e0b8e
gobeyye <3325229319@qq.com>
2024-03-22 19:52:12 +0800

    2023 03 22日常

---
+++ _2024 03 22/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ${USER}
+ * Date: ${YEAR}-${MONTH}-${DAY}
+ * Time: ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+    }
+}
