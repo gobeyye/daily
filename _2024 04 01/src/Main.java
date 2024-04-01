bb2ae7d58a9127991effcd6ee54885c5b7d45c5d
gobeyye <3325229319@qq.com>
2024-04-01 23:32:35 +0800

    2024 04 01日常

---
+++ _2024 04 01/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ${USER}
+ * Date: ${YEAR}-${MONTH}-${DAY}
+ * Time: ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        int a = -2 % 3 + 3;
+        System.out.println(a);
+    }
+}
