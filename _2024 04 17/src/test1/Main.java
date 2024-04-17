ece3c01b2e213f56a1dbef52f24a74fc76bfcd66
gobeyye <3325229319@qq.com>
2024-04-17 23:20:52 +0800

    2024 04 17日常

---
+++ _2024 04 17/src/test1/Main.java
@@ -0,0 +1,59 @@
+package test1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: HXL
+ * Date: 2024-04-17
+ * Time: 19:28
+ */
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    public static Read in = new Read();
+
+    public static void main(String[] args) throws IOException
+    {
+        // 写代码
+
+        out.close();
+    }
+}
+
+
+class Read // 自定义快速读入
+{
+    StringTokenizer st = new StringTokenizer("");
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    String next() throws IOException
+    {
+        while(!st.hasMoreTokens())
+        {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    String nextLine() throws IOException
+    {
+        return bf.readLine();
+    }
+
+    int nextInt() throws IOException
+    {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() throws IOException
+    {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() throws IOException
+    {
+        return Double.parseDouble(next());
+    }
+}
---
+++ _2024 04 17/src/test2/Main.java
@@ -0,0 +1,16 @@
+package test2;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: HXL
+ * Date: 2024-04-17
+ * Time: 21:01
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+
+
+    }
+}
